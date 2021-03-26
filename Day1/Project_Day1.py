aa9066162e67fcab0f38d6a0d6308413eb18f85e
hasan032 <smmahamudul32@student.sust.edu>
2021-03-26 23:24:24 +0600

    1st date commit

---
+++ Day1/Project_Day1.py
@@ -0,0 +1,14 @@
+#Welcome to the Brand Name Gemerator
+#What's name of the city you grew up in?
+#Bristol
+#What's Your pet name?
+#Rabbit
+#Your Brand Name could be Bristol Rabbit
+
+print("Welcome to the Brand Name Generator. ")
+City= (input("What is the name of city you grew up in ?\n  : "))
+Pet = (input("What is your pet name? \n  :"))
+print("Your brand name could be " + City + " "+ Pet  )
+
+
+#first date project done.
---
+++ Day1/basic.py
@@ -0,0 +1,12 @@
+
+
+#Day 1 - String Manipulation
+#String Concataniation is done with the "+" sign
+#e.g. print("Hello World")
+#New line can be connected with a backslash and n.
+
+print("Day 1 - String Manipulation")
+print('String Concataniation is done with the "+" sign')
+print('e.g. print("Hello World")')
+print("New line can be connected with a backslash and n.")
+
