34d1b0c54d51e32613187e59597a4285294167b0
hasan032 <smmahamudul32@student.sust.edu>
2021-04-07 21:12:34 +0600

    roller coster ticket update

--- day3/rollar_coster_ticket.py
+++ day3/rollar_coster_ticket.py
@@ -4,24 +4,45 @@ height = int(input("What is your height in cm? : "))
 if height >= 120:
     print("You can ride rollarcoster!")
     age = int (input("what is your age ? : "))
-    photo =str(input("Do you want to take photos ? y/n  :  "))
+    photo =(input("Do you want to take photos ? Y or N  :  "))
     
+    bill = 0
 
-    if photo == "y":
-        if age< 12:
-            print("Please pay 8 dollar")
-        elif age<=18:
-            print("Please pay 10 dollar.")
-        else:
-            print("please pay 15 dollar. ")
 
+    if age < 12:
+        bill = 5
+    elif age < 18:
+        bill = 7
+    elif age >=45 and age<=55:
+        print("Everything is okay. Your ride is free now ")
     else:
-        if age< 12:
-            print("Please pay 5 dollar")
-        elif age<=18:
-            print("Please pay 7 dollar.")
-        else:
-            print("please pay 12 dollar. ")
+        bill = 12
+
+
+    if photo== "Y":
+        bill += 3
+    else:
+        bill +=  0
+
+    print(f"Your total bill is {bill} ")   
+    
+    
+    
+    # if photo == "y":
+    #     if age< 12:
+    #         print("Please pay 8 dollar")
+    #     elif age<=18:
+    #         print("Please pay 10 dollar.")
+    #     else:
+    #         print("please pay 15 dollar. ")
+
+    # else:
+    #     if age< 12:
+    #         print("Please pay 5 dollar")
+    #     elif age<=18:
+    #         print("Please pay 7 dollar.")
+    #     else:
+    #         print("please pay 12 dollar. ")
 
 
 
