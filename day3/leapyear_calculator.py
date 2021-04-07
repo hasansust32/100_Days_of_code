8a1e26caa952833a3c431fe7a644c85e51229bb0
hasan032 <smmahamudul32@student.sust.edu>
2021-04-07 13:13:57 +0600

    commit

--- day3/leapyear_calculator.py
+++ day3/leapyear_calculator.py
@@ -7,4 +7,16 @@ year = int(input("enter a year you want to check : "))
 
 #okay this year is 2020
 
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("This is a leap year .")
+        else:
+            print("Not leap year .")
+    else:
+        print("This is leap year :")
+else:
+    print("This is not leap year. ")
+
+
 
