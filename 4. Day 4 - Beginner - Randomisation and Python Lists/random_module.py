7254ab998c68d3b914ba9eeab26d0f13656e94cb
hasan032 <smmahamudul32@student.sust.edu>
2021-04-09 10:29:39 +0600

    test random

---
+++ 4. Day 4 - Beginner - Randomisation and Python Lists/random_module.py
@@ -0,0 +1,23 @@
+
+import random
+# import my_module
+
+# random_number = random.randint(1,100)
+
+# random_float = random.random()
+# # print(random_number)
+
+# print(random_float)
+# print(my_module.pi)
+
+# print(f"Your love score is {random_number} ")
+
+# test_sheed = int(input("create a sheed number to check  :"))
+# print(random.seed(test_sheed)
+
+random_sheet = random.randint(0,1)
+if random_sheet==1:
+    print("Heads")
+else:
+    print("Tail")
+
