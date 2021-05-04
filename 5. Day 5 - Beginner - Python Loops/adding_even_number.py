1953a776d9fb474c0e88874653b178086bbb3949
SM Mahamudul Hasan <smmahamudul32@student.sust.edu>
2021-05-04 09:42:42 +0600

    even number add

---
+++ 5. Day 5 - Beginner - Python Loops/adding_even_number.py
@@ -0,0 +1,19 @@
+#In this file , i will add even numbers and their sum.
+
+total = 0
+for number in range (1, 101 , 2):
+    total += number
+print(total)
+
+
+total2 =0
+for number in range (1,101):
+    if number %  2 == 0:
+        total2 += number
+
+print(total2)
+
+
+
+
+
