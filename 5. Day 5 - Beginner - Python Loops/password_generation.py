f0673e8d4394ec3f40fb36e421f3056efc8e9ce6
SM Mahamudul Hasan <smmahamudul32@student.sust.edu>
2021-05-04 22:22:36 +0600

    commit

---
+++ 5. Day 5 - Beginner - Python Loops/password_generation.py
@@ -0,0 +1,18 @@
+#password generation python file is here.
+
+print("Welcome to the pypassword Generator!")
+letter = int(input("How many letters would you like in your password ? : \n"))
+symbol= int (input("How many symbols would you like  ? : \n"))
+numbers = int (input("How many numbers would you like ? : \n"))
+
+
+
+
+
+
+
+
+
+
+
+
