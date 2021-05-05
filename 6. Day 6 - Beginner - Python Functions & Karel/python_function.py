106f21542cc5f98aa1090f359dfacd76a969af2f
SM Mahamudul Hasan <smmahamudul32@student.sust.edu>
2021-05-05 13:32:38 +0600

    python functions

---
+++ 6. Day 6 - Beginner - Python Functions & Karel/python_function.py
@@ -0,0 +1,19 @@
+#declaring python function ...
+
+# print("hello")
+# my_char = len("hello")
+# print(my_char)
+
+def my_function():
+    
+    print("Hello")
+    print("bye ")
+
+my_function()
+
+
+
+
+
+
+
---
+++ 6. Day 6 - Beginner - Python Functions & Karel/robotics_move.py
@@ -0,0 +1,29 @@
+#Reebogs world ......!
+
+#this is from the URL : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+#the code is now given:
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# def one_cycle():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+
+# for step in range(4):
+#     one_cycle()
+
+
+
+
+
+
